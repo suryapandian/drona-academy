6d520769eb5e7c0548a71690a1d1460887d06947
Surya <suryamailsyou@gmail.com>
2020-03-12 13:28:58 +0530

    Set environment variable

--- static.config.js
+++ static.config.js
@@ -3,7 +3,7 @@ import Document from './src/Document'
 
 export default {
   Document,
-  siteRoot: 'dist',
+  siteRoot: process.env.SITE_ROOT,
   getSiteData: async () => {
     const portfolioLinks = await getPortfolioLinks()
     return {
